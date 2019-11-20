ca7f6ef0a5044eb2ae0517259428a3c0c528e0ca
Manuel Strebel <strebel@novu.ch>
2019-11-20 22:07:26 +0100

    add api response

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Computer;
 use Illuminate\Http\Request;
 
 /*
@@ -15,4 +16,21 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/72c112c55cb41d20df06b27e535428c78bee804d', function (\Phpwol\Factory $phpwol) {
+    $magicPacket = $phpwol->magicPacket();
+    $computer = Computer::find(1);
+
+    if ($computer->use_broadcast) {
+        $result = $magicPacket->send($computer->mac, $computer->broadcast);
+    } else {
+        $result = $magicPacket->send($computer->mac, $computer->ip, $computer->subnet);
+    }
+
+    if ($result) {
+        return response('Done', 200);
+    } else {
+        return response('Boot request failed', 503);
+    }
 });
